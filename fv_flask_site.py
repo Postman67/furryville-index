d4d815564b92d62dc14faeae6d27d94f50de9824
Postman67 <43255710+Postman67@users.noreply.github.com>
2026-01-17 15:30:29 -0800

    Added proper handling and routing for the 404 page

--- fv_flask_site.py
+++ fv_flask_site.py
@@ -322,5 +322,10 @@ def health():
         "service": "furryville-index"
     })
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """Handle 404 errors by serving custom 404 page"""
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
